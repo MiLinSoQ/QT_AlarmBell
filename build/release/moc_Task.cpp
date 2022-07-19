58fe88ba60ef7494716b4a308cce14dfa6413117
MiLinSoQ <bluejacky258@gmail.com>
2022-07-19 11:37:58 +0800

    Release

---
+++ build/release/moc_Task.cpp
@@ -0,0 +1,151 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'Task.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../src/Task.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'Task.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Task_t {
+    QByteArrayData data[8];
+    char stringdata0[108];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Task_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Task_t qt_meta_stringdata_Task = {
+    {
+QT_MOC_LITERAL(0, 0, 4), // "Task"
+QT_MOC_LITERAL(1, 5, 17), // "AudioMediaChanged"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 13), // "QMediaContent"
+QT_MOC_LITERAL(4, 38, 20), // "AudioPositionChanged"
+QT_MOC_LITERAL(5, 59, 18), // "AudioStatusChanged"
+QT_MOC_LITERAL(6, 78, 19), // "QMediaPlayer::State"
+QT_MOC_LITERAL(7, 98, 9) // "AudioStop"
+
+    },
+    "Task\0AudioMediaChanged\0\0QMediaContent\0"
+    "AudioPositionChanged\0AudioStatusChanged\0"
+    "QMediaPlayer::State\0AudioStop"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Task[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   34,    2, 0x0a /* Public */,
+       4,    1,   37,    2, 0x0a /* Public */,
+       5,    1,   40,    2, 0x0a /* Public */,
+       7,    0,   43,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    2,
+    QMetaType::Void, QMetaType::LongLong,    2,
+    QMetaType::Void, 0x80000000 | 6,    2,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Task::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Task *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->AudioMediaChanged((*reinterpret_cast< QMediaContent(*)>(_a[1]))); break;
+        case 1: _t->AudioPositionChanged((*reinterpret_cast< qint64(*)>(_a[1]))); break;
+        case 2: _t->AudioStatusChanged((*reinterpret_cast< QMediaPlayer::State(*)>(_a[1]))); break;
+        case 3: _t->AudioStop(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaContent >(); break;
+            }
+            break;
+        case 2:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QMediaPlayer::State >(); break;
+            }
+            break;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Task::staticMetaObject = { {
+    &QObject::staticMetaObject,
+    qt_meta_stringdata_Task.data,
+    qt_meta_data_Task,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Task::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Task::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Task.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Task::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
